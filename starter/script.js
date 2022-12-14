369e8d29f0c3034d914c6ec2c1bf9bd62d2fcf69
Petros Tsakonas <petros-tsakonas@hotmail.com>
2022-12-14 14:15:08 +0200

    Call, Apply and Bind Methods
    Manipulating where the this keyword points to, as well as passing arguments and binding certain ones. Eg. binding the tax rate at 0.23 for VAT for calculating final value including VAT.

--- starter/script.js
+++ starter/script.js
@@ -183,5 +183,30 @@ lufthansa.buyPlane = function() {
   console.log(this);
   
   this.planes++
-
+  console.log(this.planes);
 }
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Challenge
+
+const addTAXrate = function(rate) {
+  return function(value) {
+    console.log(value + value * rate);
+  }
+}
+
+const addVAT2 = addTAXch(0.23);
+addVATch(100);
+
+const addVAT2Arrow = rate => value => console.log(value + value * rate);
+addVAT2Arrow(0.23)(23);
